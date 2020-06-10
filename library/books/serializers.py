aa4da49bce63cecf600dec695f3aa985dd86a9f3
BartoszCzernal <bartekczernal@gmail.com>
2020-06-10 12:49:48 +0200

    Create nested serialization in BookSerializer

--- library/books/serializers.py
+++ library/books/serializers.py
@@ -3,12 +3,6 @@ from rest_framework.serializers import ModelSerializer
 from .models import Book, Author, Category
 
 
-class BookSerializer(ModelSerializer):
-    class Meta:
-        model = Book
-        fields = '__all__'
-
-
 class AuthorSerializer(ModelSerializer):
     class Meta:
         model = Author
@@ -19,3 +13,12 @@ class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+
+class BookSerializer(ModelSerializer):
+    authors = AuthorSerializer(many=True, read_only=True)
+    categories = CategorySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Book
+        fields = ['id', 'isbn', 'title', 'date_of_release', 'authors', 'categories', 'on_loan_to']
