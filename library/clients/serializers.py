c8e8cf420a1214a8e1dab02b8cfddd5200683f03
BartoszCzernal <bartekczernal@gmail.com>
2020-06-15 13:16:39 +0200

    Create nested serialization in ClientSerializer and RentalSerializer

--- library/clients/serializers.py
+++ library/clients/serializers.py
@@ -1,12 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 
 from .models import Client, Address, Rental
-
-
-class ClientSerializer(ModelSerializer):
-    class Meta:
-        model = Client
-        fields = '__all__'
+from books.serializers import BookSerializer
 
 
 class AddressSerializer(ModelSerializer):
@@ -15,7 +10,25 @@ class AddressSerializer(ModelSerializer):
         fields = '__all__'
 
 
+class ClientSerializer(ModelSerializer):
+    address = AddressSerializer(read_only=False)
+
+    def create(self, validated_data):
+        address_data = validated_data.pop('address')
+        client = Client.objects.create(**validated_data)
+        address, created = Address.objects.get_or_create(**address_data)
+        client.address = address
+        return client
+
+    class Meta:
+        model = Client
+        fields = ['id', 'first_name', 'last_name', 'date_of_birth', 'email', 'phone', 'address']
+
+
 class RentalSerializer(ModelSerializer):
+    client = ClientSerializer(read_only=True)
+    book = BookSerializer(read_only=True)
+
     class Meta:
         model = Rental
-        fields = "__all__"
+        fields = '__all__'
